170f923ec9038539ea77c4d11b9f1baf6f65ce43
MagenX <magenx@users.noreply.github.com>
2025-02-16 22:23:23 +0100

    Update permissions.sh

--- deploy/permissions.sh
+++ deploy/permissions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # before install
+# get latest release folder
+NEW_RELEASE="/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive"
 # set permissions
-chown -hR ${BRAND}:php-${BRAND} "\${NEW_RELEASE}"
-chmod 2750 ${ROOT_PATH} "\${NEW_RELEASE}"
-setfacl -R -m m:r-X,u:${BRAND}:rwX,g:${PHP_USER}:r-X,o::-,d:u:${BRAND}:rwX,d:g:${PHP_USER}:r-X,d:o::- "\${NEW_RELEASE}"
-setfacl -R -m u:${OWNER}:rwX,g:${PHP_USER}:rwX,o::-,d:u:${OWNER}:rwX,d:g:${PHP_USER}:rwX,d:o::- "\${NEW_RELEASE}"/{var,pub/media}
-setfacl -R -m u:nginx:r-X,d:u:nginx:r-X "\${NEW_RELEASE}"
+chown -R ${BRAND}:php-${BRAND} ${NEW_RELEASE}
+chmod 2750 ${NEW_RELEASE}
+setfacl -R -m m:r-X,u:${BRAND}:rwX,g:${PHP_USER}:r-X,o::-,d:u:${BRAND}:rwX,d:g:${PHP_USER}:r-X,d:o::- ${NEW_RELEASE}
+setfacl -R -m u:${BRAND}:rwX,g:${PHP_USER}:rwX,o::-,d:u:${BRAND}:rwX,d:g:${PHP_USER}:rwX,d:o::- ${NEW_RELEASE}/{var,pub/media}
+setfacl -R -m u:nginx:r-X,d:u:nginx:r-X "${NEW_RELEASE}"
